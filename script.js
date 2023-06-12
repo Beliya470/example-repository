83a3ed2e818179676de332a126a6ec73e13dc3d9
Beliya470 <anneanziay78@gmail.com>
2023-06-12 01:28:08 -0700

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thank you for your message, ${name}! I will get back to you at ${email} soon.`);
+});
